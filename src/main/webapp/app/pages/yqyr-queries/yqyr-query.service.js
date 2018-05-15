01ca4477c0ca416fe4a65ae05689d4dd92acf52f
laurens <laurensius.sakti@atibusinessgroup.com>
2018-05-15 10:28:39 +0700

    yqyr ui

---
+++ src/main/webapp/app/pages/yqyr-queries/yqyr-query.service.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+    angular
+        .module('fmpApp')
+        .factory('YqyrQuery', YqyrQuery);
+
+    YqyrQuery.$inject = ['$resource'];
+
+    function YqyrQuery ($resource) {
+        var resourceUrl =  'api/yqyr-queries/:id';
+
+        return $resource(resourceUrl, {}, {
+            'query': { method: 'GET', isArray: true},
+            'get': {
+                method: 'GET',
+                transformResponse: function (data) {
+                    if (data) {
+                        data = angular.fromJson(data);
+                    }
+                    return data;
+                }
+            },
+            'update': { method:'PUT' }
+        });
+    }
+})();
