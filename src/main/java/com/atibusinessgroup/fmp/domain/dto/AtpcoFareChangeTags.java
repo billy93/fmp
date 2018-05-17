727fbe2f3524341ad524566e79c8054ab9554f26
Tenu Tanaji Wilmar <tenu.tanaji@atibusinessgroup.com>
2018-05-17 09:35:17 +0700

    AFD_Query

---
+++ src/main/java/com/atibusinessgroup/fmp/domain/dto/AtpcoFareChangeTags.java
@@ -0,0 +1,393 @@
+package com.atibusinessgroup.fmp.domain.dto;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+
+public class AtpcoFareChangeTags {
+	
+	@Field("change_tags_1")
+	private String tagOneNewRecord;
+	
+	@Field("change_tags_2")
+	private String tagTwoIncreasedFare;
+	
+	@Field("change_tags_3")
+	private String tagThreeReducedFare;
+	
+	@Field("change_tags_4")
+	private String tagFourCurrentlyNotBeingUsed;
+	
+	@Field("change_tags_5")
+	private String tagFiveFootnoteChange;
+	
+	@Field("change_tags_6")
+	private String tagSixRoutingMapChange;
+	
+	@Field("change_tags_7")
+	private String tagSevenReservedForFutureUse;
+	
+	@Field("change_tags_8")
+	private String tagEightMaximumPermittedMileageChange;
+	
+	@Field("change_tags_9")
+	private String tagNineReservedForFutureUse;
+	
+	@Field("change_tags_10")
+	private String tagTenEffectiveDateChange;
+	
+	@Field("change_tags_11")
+	private String tagElevenReinstatementFare;
+	
+	@Field("change_tags_12")
+	private String tagTwelveReissue;
+	
+	@Field("change_tags_13")
+	private String tagThirteenReservedForFutureUse;
+	
+	@Field("change_tags_14")
+	private String tagFourteenNoChange;
+	
+	@Field("change_tags_15")
+	private String tagFifteenCancelledRecord;
+	
+	@Field("change_tags_16")
+	private String tagSixteenNoComponentChange;
+	
+	@Field("change_tags_17")
+	private String tagSeventeenCurrencyCodeChange;
+	
+	@Field("change_tags_18")
+	private String tagEighteenFareNotSubjectToNucConversion;
+
+	public String getTagOneNewRecord() {
+		return tagOneNewRecord;
+	}
+
+	public void setTagOneNewRecord(String tagOneNewRecord) {
+		this.tagOneNewRecord = tagOneNewRecord;
+	}
+
+	public String getTagTwoIncreasedFare() {
+		return tagTwoIncreasedFare;
+	}
+
+	public void setTagTwoIncreasedFare(String tagTwoIncreasedFare) {
+		this.tagTwoIncreasedFare = tagTwoIncreasedFare;
+	}
+
+	public String getTagThreeReducedFare() {
+		return tagThreeReducedFare;
+	}
+
+	public void setTagThreeReducedFare(String tagThreeReducedFare) {
+		this.tagThreeReducedFare = tagThreeReducedFare;
+	}
+
+	public String getTagFourCurrentlyNotBeingUsed() {
+		return tagFourCurrentlyNotBeingUsed;
+	}
+
+	public void setTagFourCurrentlyNotBeingUsed(String tagFourCurrentlyNotBeingUsed) {
+		this.tagFourCurrentlyNotBeingUsed = tagFourCurrentlyNotBeingUsed;
+	}
+
+	public String getTagFiveFootnoteChange() {
+		return tagFiveFootnoteChange;
+	}
+
+	public void setTagFiveFootnoteChange(String tagFiveFootnoteChange) {
+		this.tagFiveFootnoteChange = tagFiveFootnoteChange;
+	}
+
+	public String getTagSixRoutingMapChange() {
+		return tagSixRoutingMapChange;
+	}
+
+	public void setTagSixRoutingMapChange(String tagSixRoutingMapChange) {
+		this.tagSixRoutingMapChange = tagSixRoutingMapChange;
+	}
+
+	public String getTagSevenReservedForFutureUse() {
+		return tagSevenReservedForFutureUse;
+	}
+
+	public void setTagSevenReservedForFutureUse(String tagSevenReservedForFutureUse) {
+		this.tagSevenReservedForFutureUse = tagSevenReservedForFutureUse;
+	}
+
+	public String getTagEightMaximumPermittedMileageChange() {
+		return tagEightMaximumPermittedMileageChange;
+	}
+
+	public void setTagEightMaximumPermittedMileageChange(String tagEightMaximumPermittedMileageChange) {
+		this.tagEightMaximumPermittedMileageChange = tagEightMaximumPermittedMileageChange;
+	}
+
+	public String getTagNineReservedForFutureUse() {
+		return tagNineReservedForFutureUse;
+	}
+
+	public void setTagNineReservedForFutureUse(String tagNineReservedForFutureUse) {
+		this.tagNineReservedForFutureUse = tagNineReservedForFutureUse;
+	}
+
+	public String getTagTenEffectiveDateChange() {
+		return tagTenEffectiveDateChange;
+	}
+
+	public void setTagTenEffectiveDateChange(String tagTenEffectiveDateChange) {
+		this.tagTenEffectiveDateChange = tagTenEffectiveDateChange;
+	}
+
+	public String getTagElevenReinstatementFare() {
+		return tagElevenReinstatementFare;
+	}
+
+	public void setTagElevenReinstatementFare(String tagElevenReinstatementFare) {
+		this.tagElevenReinstatementFare = tagElevenReinstatementFare;
+	}
+
+	public String getTagTwelveReissue() {
+		return tagTwelveReissue;
+	}
+
+	public void setTagTwelveReissue(String tagTwelveReissue) {
+		this.tagTwelveReissue = tagTwelveReissue;
+	}
+
+	public String getTagThirteenReservedForFutureUse() {
+		return tagThirteenReservedForFutureUse;
+	}
+
+	public void setTagThirteenReservedForFutureUse(String tagThirteenReservedForFutureUse) {
+		this.tagThirteenReservedForFutureUse = tagThirteenReservedForFutureUse;
+	}
+
+	public String getTagFourteenNoChange() {
+		return tagFourteenNoChange;
+	}
+
+	public void setTagFourteenNoChange(String tagFourteenNoChange) {
+		this.tagFourteenNoChange = tagFourteenNoChange;
+	}
+
+	public String getTagFifteenCancelledRecord() {
+		return tagFifteenCancelledRecord;
+	}
+
+	public void setTagFifteenCancelledRecord(String tagFifteenCancelledRecord) {
+		this.tagFifteenCancelledRecord = tagFifteenCancelledRecord;
+	}
+
+	public String getTagSixteenNoComponentChange() {
+		return tagSixteenNoComponentChange;
+	}
+
+	public void setTagSixteenNoComponentChange(String tagSixteenNoComponentChange) {
+		this.tagSixteenNoComponentChange = tagSixteenNoComponentChange;
+	}
+
+	public String getTagSeventeenCurrencyCodeChange() {
+		return tagSeventeenCurrencyCodeChange;
+	}
+
+	public void setTagSeventeenCurrencyCodeChange(String tagSeventeenCurrencyCodeChange) {
+		this.tagSeventeenCurrencyCodeChange = tagSeventeenCurrencyCodeChange;
+	}
+
+	public String getTagEighteenFareNotSubjectToNucConversion() {
+		return tagEighteenFareNotSubjectToNucConversion;
+	}
+
+	public void setTagEighteenFareNotSubjectToNucConversion(String tagEighteenFareNotSubjectToNucConversion) {
+		this.tagEighteenFareNotSubjectToNucConversion = tagEighteenFareNotSubjectToNucConversion;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((tagEightMaximumPermittedMileageChange == null) ? 0
+				: tagEightMaximumPermittedMileageChange.hashCode());
+		result = prime * result + ((tagEighteenFareNotSubjectToNucConversion == null) ? 0
+				: tagEighteenFareNotSubjectToNucConversion.hashCode());
+		result = prime * result + ((tagElevenReinstatementFare == null) ? 0 : tagElevenReinstatementFare.hashCode());
+		result = prime * result + ((tagFifteenCancelledRecord == null) ? 0 : tagFifteenCancelledRecord.hashCode());
+		result = prime * result + ((tagFiveFootnoteChange == null) ? 0 : tagFiveFootnoteChange.hashCode());
+		result = prime * result
+				+ ((tagFourCurrentlyNotBeingUsed == null) ? 0 : tagFourCurrentlyNotBeingUsed.hashCode());
+		result = prime * result + ((tagFourteenNoChange == null) ? 0 : tagFourteenNoChange.hashCode());
+		result = prime * result + ((tagNineReservedForFutureUse == null) ? 0 : tagNineReservedForFutureUse.hashCode());
+		result = prime * result + ((tagOneNewRecord == null) ? 0 : tagOneNewRecord.hashCode());
+		result = prime * result
+				+ ((tagSevenReservedForFutureUse == null) ? 0 : tagSevenReservedForFutureUse.hashCode());
+		result = prime * result
+				+ ((tagSeventeenCurrencyCodeChange == null) ? 0 : tagSeventeenCurrencyCodeChange.hashCode());
+		result = prime * result + ((tagSixRoutingMapChange == null) ? 0 : tagSixRoutingMapChange.hashCode());
+		result = prime * result + ((tagSixteenNoComponentChange == null) ? 0 : tagSixteenNoComponentChange.hashCode());
+		result = prime * result + ((tagTenEffectiveDateChange == null) ? 0 : tagTenEffectiveDateChange.hashCode());
+		result = prime * result
+				+ ((tagThirteenReservedForFutureUse == null) ? 0 : tagThirteenReservedForFutureUse.hashCode());
+		result = prime * result + ((tagThreeReducedFare == null) ? 0 : tagThreeReducedFare.hashCode());
+		result = prime * result + ((tagTwelveReissue == null) ? 0 : tagTwelveReissue.hashCode());
+		result = prime * result + ((tagTwoIncreasedFare == null) ? 0 : tagTwoIncreasedFare.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		AtpcoFareChangeTags other = (AtpcoFareChangeTags) obj;
+		if (tagEightMaximumPermittedMileageChange == null) {
+			if (other.tagEightMaximumPermittedMileageChange != null) {
+				return false;
+			}
+		} else if (!tagEightMaximumPermittedMileageChange.equals(other.tagEightMaximumPermittedMileageChange)) {
+			return false;
+		}
+		if (tagEighteenFareNotSubjectToNucConversion == null) {
+			if (other.tagEighteenFareNotSubjectToNucConversion != null) {
+				return false;
+			}
+		} else if (!tagEighteenFareNotSubjectToNucConversion.equals(other.tagEighteenFareNotSubjectToNucConversion)) {
+			return false;
+		}
+		if (tagElevenReinstatementFare == null) {
+			if (other.tagElevenReinstatementFare != null) {
+				return false;
+			}
+		} else if (!tagElevenReinstatementFare.equals(other.tagElevenReinstatementFare)) {
+			return false;
+		}
+		if (tagFifteenCancelledRecord == null) {
+			if (other.tagFifteenCancelledRecord != null) {
+				return false;
+			}
+		} else if (!tagFifteenCancelledRecord.equals(other.tagFifteenCancelledRecord)) {
+			return false;
+		}
+		if (tagFiveFootnoteChange == null) {
+			if (other.tagFiveFootnoteChange != null) {
+				return false;
+			}
+		} else if (!tagFiveFootnoteChange.equals(other.tagFiveFootnoteChange)) {
+			return false;
+		}
+		if (tagFourCurrentlyNotBeingUsed == null) {
+			if (other.tagFourCurrentlyNotBeingUsed != null) {
+				return false;
+			}
+		} else if (!tagFourCurrentlyNotBeingUsed.equals(other.tagFourCurrentlyNotBeingUsed)) {
+			return false;
+		}
+		if (tagFourteenNoChange == null) {
+			if (other.tagFourteenNoChange != null) {
+				return false;
+			}
+		} else if (!tagFourteenNoChange.equals(other.tagFourteenNoChange)) {
+			return false;
+		}
+		if (tagNineReservedForFutureUse == null) {
+			if (other.tagNineReservedForFutureUse != null) {
+				return false;
+			}
+		} else if (!tagNineReservedForFutureUse.equals(other.tagNineReservedForFutureUse)) {
+			return false;
+		}
+		if (tagOneNewRecord == null) {
+			if (other.tagOneNewRecord != null) {
+				return false;
+			}
+		} else if (!tagOneNewRecord.equals(other.tagOneNewRecord)) {
+			return false;
+		}
+		if (tagSevenReservedForFutureUse == null) {
+			if (other.tagSevenReservedForFutureUse != null) {
+				return false;
+			}
+		} else if (!tagSevenReservedForFutureUse.equals(other.tagSevenReservedForFutureUse)) {
+			return false;
+		}
+		if (tagSeventeenCurrencyCodeChange == null) {
+			if (other.tagSeventeenCurrencyCodeChange != null) {
+				return false;
+			}
+		} else if (!tagSeventeenCurrencyCodeChange.equals(other.tagSeventeenCurrencyCodeChange)) {
+			return false;
+		}
+		if (tagSixRoutingMapChange == null) {
+			if (other.tagSixRoutingMapChange != null) {
+				return false;
+			}
+		} else if (!tagSixRoutingMapChange.equals(other.tagSixRoutingMapChange)) {
+			return false;
+		}
+		if (tagSixteenNoComponentChange == null) {
+			if (other.tagSixteenNoComponentChange != null) {
+				return false;
+			}
+		} else if (!tagSixteenNoComponentChange.equals(other.tagSixteenNoComponentChange)) {
+			return false;
+		}
+		if (tagTenEffectiveDateChange == null) {
+			if (other.tagTenEffectiveDateChange != null) {
+				return false;
+			}
+		} else if (!tagTenEffectiveDateChange.equals(other.tagTenEffectiveDateChange)) {
+			return false;
+		}
+		if (tagThirteenReservedForFutureUse == null) {
+			if (other.tagThirteenReservedForFutureUse != null) {
+				return false;
+			}
+		} else if (!tagThirteenReservedForFutureUse.equals(other.tagThirteenReservedForFutureUse)) {
+			return false;
+		}
+		if (tagThreeReducedFare == null) {
+			if (other.tagThreeReducedFare != null) {
+				return false;
+			}
+		} else if (!tagThreeReducedFare.equals(other.tagThreeReducedFare)) {
+			return false;
+		}
+		if (tagTwelveReissue == null) {
+			if (other.tagTwelveReissue != null) {
+				return false;
+			}
+		} else if (!tagTwelveReissue.equals(other.tagTwelveReissue)) {
+			return false;
+		}
+		if (tagTwoIncreasedFare == null) {
+			if (other.tagTwoIncreasedFare != null) {
+				return false;
+			}
+		} else if (!tagTwoIncreasedFare.equals(other.tagTwoIncreasedFare)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "AtpcoFareChangeTags [tagOneNewRecord=" + tagOneNewRecord + ", tagTwoIncreasedFare="
+				+ tagTwoIncreasedFare + ", tagThreeReducedFare=" + tagThreeReducedFare
+				+ ", tagFourCurrentlyNotBeingUsed=" + tagFourCurrentlyNotBeingUsed + ", tagFiveFootnoteChange="
+				+ tagFiveFootnoteChange + ", tagSixRoutingMapChange=" + tagSixRoutingMapChange
+				+ ", tagSevenReservedForFutureUse=" + tagSevenReservedForFutureUse
+				+ ", tagEightMaximumPermittedMileageChange=" + tagEightMaximumPermittedMileageChange
+				+ ", tagNineReservedForFutureUse=" + tagNineReservedForFutureUse + ", tagTenEffectiveDateChange="
+				+ tagTenEffectiveDateChange + ", tagElevenReinstatementFare=" + tagElevenReinstatementFare
+				+ ", tagTwelveReissue=" + tagTwelveReissue + ", tagThirteenReservedForFutureUse="
+				+ tagThirteenReservedForFutureUse + ", tagFourteenNoChange=" + tagFourteenNoChange
+				+ ", tagFifteenCancelledRecord=" + tagFifteenCancelledRecord + ", tagSixteenNoComponentChange="
+				+ tagSixteenNoComponentChange + ", tagSeventeenCurrencyCodeChange=" + tagSeventeenCurrencyCodeChange
+				+ ", tagEighteenFareNotSubjectToNucConversion=" + tagEighteenFareNotSubjectToNucConversion + "]";
+	}
+}
