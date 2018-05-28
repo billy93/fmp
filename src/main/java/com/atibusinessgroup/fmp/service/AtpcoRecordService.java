c563e73d775c5f9ce78ac87b992a59614d4f6007
Tenu Tanaji Wilmar <tenu.tanaji@atibusinessgroup.com>
2018-05-28 12:31:31 +0700

    atpco_record

--- src/main/java/com/atibusinessgroup/fmp/service/AtpcoRecordService.java
+++ src/main/java/com/atibusinessgroup/fmp/service/AtpcoRecordService.java
@@ -7,7 +7,11 @@ public class AtpcoRecordService {
 
 	public boolean compareMatchingFareAndRecord(String fGeoType1, String fGeoLoc1, String fGeoType2, String fGeoLoc2, String fOwrt, String fRoutingNo, String fFootnote, Object fEffectiveDate, Object fDiscontinueDate, 
 			String rGeoType1, String rGeoLoc1, String rGeoType2, String rGeoLoc2, String rOwrt, String rRoutingNo, String rFootnote, Object rEffectiveDate, Object rDiscontinueDate) {
-		boolean match = false;
+		boolean match = true;
+		
+		System.out.println(fGeoType1 + " " + fGeoLoc1 + " " + fGeoType2 + " " + fGeoLoc2 + " " + fOwrt + " " + fRoutingNo + " " + fFootnote + " " + fEffectiveDate + " " + fDiscontinueDate);
+		System.out.println(rGeoType1 + " " + rGeoLoc1 + " " + rGeoType2 + " " + rGeoLoc2 + " " + rOwrt + " " + rRoutingNo + " " + rFootnote + " " + rEffectiveDate + " " + rDiscontinueDate);
+		
 		
 		
 		return match;
