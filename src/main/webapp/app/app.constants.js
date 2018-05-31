d5b623e1c33548b834701af1dd8ed1406e41cd61
billy93 <andreas.sutandi@atibusinessgroup.com>
2018-05-31 12:33:30 +0700

    Prod Test

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('fmpApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1527744144437)
+        .constant('BUILD_TIMESTAMP', 1527744724284)
 ;
 })();
