625764940e8bd58d3ff77c76c8c48e258cc347b9
billy93 <andreas.sutandi@atibusinessgroup.com>
2018-06-11 07:50:50 +0700

    Fix WP

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('fmpApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1528677063250)
+        .constant('BUILD_TIMESTAMP', 1528678019192)
 ;
 })();
